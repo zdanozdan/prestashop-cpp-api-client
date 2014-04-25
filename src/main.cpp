7654810d361331fc0e1602d94b699b169bc99c65
tomi707 <tomi707@wp.pl>
2014-04-25 16:14:49 +0200

    1.2
    - poprawiony adres URL, dodane 'www'

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
     PrestaApi prestaApi;
-    prestaApi.initApi("RB9WCBTSVZCZOASIJ1LUBAVII1PS8IGE", "", "http://presta.mikran.pl/prestashop/api");
+    prestaApi.initApi("RB9WCBTSVZCZOASIJ1LUBAVII1PS8IGE", "", "http://www.presta.mikran.pl/prestashop/api");
 
     vector<string> categories;
     //categories.push_back("Kategoria_1");
